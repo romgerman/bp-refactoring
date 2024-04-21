5751fd5868bc90fdc83127808e1ca33980bb0d21
Aleksandr Chashchin <achasch@gmail.com>
2024-04-21 12:49:21 +0200

    add regexp evaluate

--- src/blueprint/nodes/data/constantString.ts
+++ src/blueprint/nodes/data/constantString.ts
@@ -5,7 +5,13 @@ export class ConstantStringNode extends BlueprintNode<{ value: string }> {
   readonly type: string = NodeTypes.ConstantString;
 
   async evaluate(): Promise<any> {
-    return this.state?.value;
+    try {
+      const regex = new RegExp(this.state?.value || "");
+      const predicate = (data: string) => regex.test(data);
+      return predicate;
+    } catch {
+      throw new Error("Incorrect regular expression");
+    }
   }
 
   getViewData(): Promise<any> {
