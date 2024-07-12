d4dbfe2078b21ba5403a5a43f10c66a742405d43
German <romgerman@yandex.ru>
2024-07-12 19:44:06 +0300

    function list dialog

--- src/webview/nodes/aggregation/FunctionListNode.vue
+++ src/webview/nodes/aggregation/FunctionListNode.vue
@@ -1,6 +1,6 @@
 <template>
-  <NodeWrapper>
-    <template #header>Functions</template>
+  <NodeWrapper @dblclick="openModal()">
+    <template #header>Functions ({{ model.allItems.length }})</template>
     <template #body>
       <div class="nowheel">
         <div style="max-height: 200px; overflow: auto">
@@ -18,18 +18,44 @@
       <Handle id="0:array" type="target" connectable="single" :position="Position.Left" data-name="Array" />
     </div>
     <Handle id="0:array" type="source" :position="Position.Right" />
+
+    <Teleport to="body">
+      <Dialog ref="editorModal">
+        <template #header>
+          <vscode-text-field placeholder="Search..." v-model="editorState.query"></vscode-text-field>
+        </template>
+        <table class="table table-sm">
+          <tbody>
+            <tr class="hover" v-for="fn in filteredItems">
+              <td>{{ fn }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </Dialog>
+    </Teleport>
   </NodeWrapper>
 </template>
 
 <script setup lang="ts">
 import { Handle, Position } from "@vue-flow/core";
 import NodeWrapper from "../../components/NodeWrapper.vue";
+import Dialog from "@/webview/components/Dialog.vue";
 import { useViewData } from "@/webview/composables/use-view-data";
 import { useCollapsableList } from "@/webview/composables/use-collapsable-list";
+import { computed, ref } from "vue";
 
-const { model, items, greaterThanMaxItems, otherItemsCount, toggleAll } = useCollapsableList();
+const { model, items, greaterThanMaxItems, otherItemsCount, toggleAll } = useCollapsableList<string>();
+const editorState = ref<{ query: string }>({
+  query: "",
+});
+const filteredItems = computed(() => model.value.allItems.filter((x) => x.includes(editorState.value.query)));
+const editorModal = ref<InstanceType<typeof Dialog>>();
 
 useViewData<string[]>((data) => {
   model.value.allItems = data;
 });
+
+function openModal(): void {
+  editorModal.value.showDialog();
+}
 </script>
